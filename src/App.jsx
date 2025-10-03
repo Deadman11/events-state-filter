2e9cfd60364b3e3411436a4385fe4df025f9ba58
Vitaliy <vindette2021@yandex.ru>
2025-10-03 23:16:58 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import './App.css'
 import Portfolio from './components/Portfolio'
 
 function App() {
     return (
-        <div className='App'>
-            <Portfolio />
-        </div>
+        <Portfolio />
     )
 }
 
